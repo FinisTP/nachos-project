fc2beb21009329c3a94a59c533b0a6a800c451a0
justindtran2001 <justindtran2001@gmail.com>
2022-02-27 19:06:13 +0700

    turn syscall help to program help (same for ascii) and few bug fixes

---
+++ code/test/help.c
@@ -0,0 +1,18 @@
+#include "syscall.h"
+
+int main()
+{
+    PrintChar('\n');
+    PrintString("<-----HELP DOCUMENT----->\n");
+    PrintChar('\n');
+    PrintString("Group members: \n");
+    PrintString("+ 19125064 - Tu Tan Phat\n");
+    PrintString("+ 19125086 - Tran Hai Duong\n");
+    PrintChar('\n');
+    PrintString("Description of the [ascii] and [sort] functions: \n");
+    PrintString("- [ascii] and [sort] are two tem calls (or functions).\n");
+    PrintString("- [ascii] allows users to see the ASCII table, including the character, decimal and hexadecimal values (Note that some characters cannot be printed out).\n");
+    PrintString("- [sort] allows users to input an array and sort them either ascending or descending.\n");
+    PrintChar('\n');
+    Halt();
+}
